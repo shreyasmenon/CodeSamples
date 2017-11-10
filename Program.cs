f92dd3f88f84c702754a8839d29786c0ce40fb12
shreyasmenon <menonshreyas14@gmail.com>
2017-11-10 14:25:20 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] arr_temp = Console.ReadLine().Split(' ');
+        int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+        arr = arr.Reverse().ToArray();
+        StringBuilder sb = new StringBuilder();
+        foreach (int arrElement in arr)
+        {
+            sb.AppendFormat("{0} ", arrElement);
+        }
+        Console.WriteLine(sb.ToString());
+        Console.ReadLine();
+    }
+}
