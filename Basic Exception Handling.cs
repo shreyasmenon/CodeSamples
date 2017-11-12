6adf9c31281e1414ebc8f4851d727655fb22cad7
shreyasmenon <menonshreyas14@gmail.com>
2017-11-12 18:00:37 -0500

    Add files via upload

---
+++ Basic Exception Handling.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankExceptionHandling
+{
+    class Program
+    {
+        //Read a string, and print its integer value;
+        //if  cannot be converted to an integer, print Bad String.
+        static void Main(string[] args)
+        {
+            string sInputString = Console.ReadLine();
+            try
+            {
+                int iConvertedInt = int.Parse(sInputString);
+                Console.Write("{0}", iConvertedInt);
+            }
+            catch (Exception ex)
+            {
+                if (ex is FormatException)
+                {
+                    Console.Write("Bad String");
+                }
+            }
+        }
+    }
+}
